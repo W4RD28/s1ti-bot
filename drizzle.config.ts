7b991d8b5767f94fdf8a3093fb8f531572c03bd2
Windu Nursetyadi <windu@localhost.localdomain>
2024-11-15 22:02:31 +0700

    add .gitignore and .env.example files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema',
-  out: './src/db/migrations',
+  out: './drizzle',
   migrations: {
     table: '_drizzle_migrations',
     schema: 'public',
